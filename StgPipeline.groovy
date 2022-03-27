1ac0d06ee86ae92e9c038f750a34f73649efbede
Vivek Tiwari <vivekcs0114@gmail.com>
2022-03-27 11:36:37 +0530

    build with params

--- StgPipeline.groovy
+++ StgPipeline.groovy
@@ -6,6 +6,10 @@ pipeline {
     FOO = "PIPELINE"
   }
 
+  parameters {
+    string(name: 'Greeting', defaultValue: 'Hello', description: 'How should I greet the world?')
+  }
+
   stages {
     stage("local") {
       environment {
@@ -14,6 +18,11 @@ pipeline {
       }
       steps {
         sh 'echo "FOO is $FOO and BAR is $BAR"'
+      }
+    }
+    stage('Greeting') {
+      steps {
+        echo "${params.Greeting} World!"
       }
     }
     stage("global") {
